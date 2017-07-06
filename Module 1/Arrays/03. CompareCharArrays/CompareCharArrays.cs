561fe772c56f4e73452e6bcc2f3fabc5706294b1
georgievlb <georgiev.lb@gmail.com>
2017-07-06 23:08:57 +0300

    first commit

---
+++ Module 1/Arrays/03. CompareCharArrays/CompareCharArrays.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string firstString = Console.ReadLine();
+        string secondString = Console.ReadLine();
+
+        char[] firstArr = new char[firstString.Length];
+        char[] secondArr = new char[secondString.Length];
+
+        for (int i = 0; i < firstArr.Length; i++)
+        {
+            firstArr[i] = firstString[i];
+        }
+
+        for (int i = 0; i < secondArr.Length; i++)
+        {
+            secondArr[i] = secondString[i];
+        }
+
+        int min = Math.Min(firstArr.Length, secondArr.Length);
+        int counter = 0;
+        do
+        {
+            if (firstArr[counter] > secondArr[counter])
+            {
+                Console.WriteLine(">");
+                break;
+            }
+            else if (firstArr[counter] < secondArr[counter])
+            {
+                Console.WriteLine("<");
+                break;
+            }
+            else if (firstArr[counter] == secondArr[counter])
+            {
+                if (counter != min)
+                {
+                    counter++;
+                    //Console.WriteLine(counter);
+                }
+                if (counter == min)
+                {
+                    if (firstArr.Length == secondArr.Length)
+                    {
+                        Console.WriteLine("=");
+                        break;
+                    }
+                    else
+                    {
+                        if (min == firstArr.Length)
+                        {
+                            Console.WriteLine("<");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine(">");
+                            break;
+                        }
+                    }
+                }
+            }
+
+        }
+        while (counter != min);
+    }
+}
+
