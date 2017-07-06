ee14f5aa040b81a351b7e6dba92c85fba90eec57
georgievlb <georgiev.lb@gmail.com>
2017-07-06 23:22:09 +0300

    array solutions

---
+++ Module 1/Arrays/01.AllocateArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class AllocateArray
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] myArray = new int[N];
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            myArray[i] = i * 5;
+        }
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            Console.WriteLine(myArray[i]);
+        }
+    }
+}
+
