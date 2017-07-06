71b9c15fc34922aad8196c6690be63f6029bb19a
georgievlb <georgiev.lb@gmail.com>
2017-07-07 02:08:23 +0300

    arrays solutions

---
+++ Module 1/Arrays/04.MaximalSequence.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] arr = new int[N];
+        
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+        int lengthOfSequence = 0;
+        int currentCounter = 1;
+
+        for (int i = 0; i < arr.Length -1; i++)
+        {
+            if (arr[i] == arr[i+1])
+            {
+                currentCounter++;                
+            }
+
+            else
+            {
+                currentCounter = 1;
+            }
+
+
+            if (currentCounter != 0 && lengthOfSequence < currentCounter)
+            {
+                lengthOfSequence = currentCounter;  //add 1 to include the first equal sequence element
+            }
+        }
+        Console.WriteLine(lengthOfSequence);
+    }
+}
