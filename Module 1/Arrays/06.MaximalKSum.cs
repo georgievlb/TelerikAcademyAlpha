779c43f17afc464a6cf955dc2a24034142472159
georgievlb <georgiev.lb@gmail.com>
2017-07-07 02:12:37 +0300

    arrays solutions

---
+++ Module 1/Arrays/06.MaximalKSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int K = int.Parse(Console.ReadLine());
+        int[] arr = new int[N];
+        int arrLength = arr.Length;
+
+        for (int i = 0; i < arrLength; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        Array.Sort(arr);
+        int sum = 0;
+        
+        for (int i = arrLength - K; i < arrLength ; i++)
+        {
+            sum += arr[i];
+        }
+        Console.WriteLine(sum);
+    }
+}
