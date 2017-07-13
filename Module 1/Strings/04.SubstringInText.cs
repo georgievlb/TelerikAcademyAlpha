0dbd587932f11d5dab1af9e47d148df269ae4287
georgievlb <georgiev.lb@gmail.com>
2017-07-13 16:35:07 +0300

    first commit

---
+++ Module 1/Strings/04.SubstringInText.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string subString = Console.ReadLine();
+        string mainString = Console.ReadLine();
+        subStringInText(subString, mainString);
+    }
+
+    static void subStringInText(string subString, string mainString)
+    {
+        int substringlength = subString.Length;
+        int counter = 0;
+        for (int i = 0; i <= mainString.Length - substringlength; i++)
+        {
+            string currentSubString = mainString.Substring(i, substringlength);
+            if (currentSubString.Equals(subString, StringComparison.CurrentCultureIgnoreCase))
+            {
+                counter++;
+            }
+        }
+        Console.WriteLine(counter);
+    }
+}
+
