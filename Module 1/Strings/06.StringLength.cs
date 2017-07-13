76303179d4deb97479f1c643b20234b77bbe2739
georgievlb <georgiev.lb@gmail.com>
2017-07-14 01:56:13 +0300

    first commit

---
+++ Module 1/Strings/06.StringLength.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        if (text.Length > 20)
+        {
+            Console.WriteLine(MaxLengthOfString(text, 20));
+        }
+
+        else
+        {
+            Console.WriteLine(text.PadRight(20, '*'));            
+        }
+        
+        
+    }
+
+    public static string MaxLengthOfString(string userInput, int length)
+    {
+        if (string.IsNullOrWhiteSpace(userInput) == false && userInput.Length > length)
+        {
+            return userInput.Substring(0, 20);            
+        }
+        return userInput;
+    }
+        
+}
