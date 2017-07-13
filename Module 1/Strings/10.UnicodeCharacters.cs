54ab689339dbebc4c8e62a7f946a75025736e894
georgievlb <georgiev.lb@gmail.com>
2017-07-14 01:54:33 +0300

    first commit

---
+++ Module 1/Strings/10.UnicodeCharacters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        StringBuilder builder = new StringBuilder();
+                
+        foreach (char letter in text)
+        {
+            builder.Append(string.Format("\\u{0:X4}", (ushort)letter));
+        }
+        Console.WriteLine(builder.ToString());
+    }
+}
