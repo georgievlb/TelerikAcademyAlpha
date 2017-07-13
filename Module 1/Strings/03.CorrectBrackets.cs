b3f6f5898053094988a0dd6999d1589ee1d7d641
georgievlb <georgiev.lb@gmail.com>
2017-07-13 16:04:56 +0300

    first commit

---
+++ Module 1/Strings/03.CorrectBrackets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string expression = Console.ReadLine();
+        int length = expression.Length;
+        int counter = 0;
+        int check = 0;
+        for (int i = 0; i < length; i++)
+        {
+            check = expression[i];
+            if (check == '(')
+            {
+                counter++;
+            }
+            else if (check == ')')
+            {
+                counter--;
+            } 
+        }
+        if (counter == 0)
+            Console.WriteLine("Correct");
+        
+        else
+            Console.WriteLine("Incorrect");
+    }    
+}
