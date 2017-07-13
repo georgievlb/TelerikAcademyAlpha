1e14cb18dded29638a3138d90ac9ca3fef8ef4eb
georgievlb <georgiev.lb@gmail.com>
2017-07-13 15:58:31 +0300

    first commit

---
+++ Module 1/Strings/02.ReverseString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string firstString = Console.ReadLine();
+        reverseString(firstString);
+    }
+
+    static void reverseString(string firstString)
+    {
+        char[] myArray = new char[firstString.Length];
+
+        for (int i = 0; i < firstString.Length; i++)
+        {
+            myArray[i] = firstString[i];
+        }
+
+        string reverseString = Convert.ToString(firstString[firstString.Length - 1]);
+
+        for (int i = firstString.Length - 2; i >= 0; i--)
+        {
+            reverseString += firstString[i];
+        }
+        Console.WriteLine(reverseString);
+    }
+}
+
