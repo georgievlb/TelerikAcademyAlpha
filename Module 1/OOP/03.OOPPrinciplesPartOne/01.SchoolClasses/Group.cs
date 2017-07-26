4761039042bbeece66688b6106fe8797d3d747d5
georgievlb <georgiev.lb@gmail.com>
2017-07-26 08:15:43 +0300

    first commit

---
+++ Module 1/OOP/03.OOPPrinciplesPartOne/01.SchoolClasses/Group.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPrinciplesPartOne
+{
+    public class Group
+    {
+        private Student Student;
+        private string GroupId;
+
+        public Group()
+        {
+
+        }
+        public Group(Student student)
+        {
+            this.Student = student;
+        }
+        public Group(string groupId)
+        {
+            this.GroupId = groupId;
+        }
+        public Group(string groupId, Student student)
+            :this(groupId)
+        {
+            this.Student = student;
+        }
+
+        public Student GetStudent   
+        {
+            get { return this.Student; }
+        }
+        public string GetGroupId
+        {
+            get { return this.GroupId; }
+            set { GroupId = value; }
+        }
+
+    }
+}
---
+++ Module 1/OOP/03.OOPPrinciplesPartOne/01.SchoolClasses/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPrinciplesPartOne
+{
+    public class Student: Human
+    {
+        private int GroupNumber;
+
+        public Student()
+            :base()
+        {
+
+        }
+
+        public Student(string name)
+            :base(name)
+        {
+
+        }
+        public Student(string name, int groupNumber)
+            :base(name)
+        {
+            this.GroupNumber = groupNumber;
+        }
+
+        public int GetGroupNo
+        {
+            get { return this.GroupNumber; }
+        }
+
+    }
+}
