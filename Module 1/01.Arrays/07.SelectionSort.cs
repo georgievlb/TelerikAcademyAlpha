b07bb7ad7bfa74bf6705c8ee68f4e2540d34d62f
georgievlb <georgiev.lb@gmail.com>
2017-10-03 14:08:46 +0300

    Added two problems for Module 1/Arrays

---
+++ Module 1/01.Arrays/07.SelectionSort.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] arr = new int[N];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int min = int.MaxValue;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                min = arr[i];
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (min > arr[j])
+                    {
+                        min = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = min;
+                    }
+                    
+                }
+            }
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
+
+
+
+class didi
+{
+    void sort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int min = arr[i];
+            for (int j = i; j < arr.Length; j++)
+            {
+                if (min > arr[j])
+                {
+                    min = arr[j];
+                }
+
+                int swap = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swap;
+            }
+        }
+    }
+}
---
+++ Module 1/01.Arrays/09.FrequentNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrequentNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 11, 90, 2, 35, 2, 2, 0, -1, -2};
+            int currentMostFrequent = 0;
+            int counter = 1;
+            currentMostFrequent = arr[0];
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                
+                if (currentMostFrequent == arr[i+1])
+                {
+                    counter++;
+                }
+
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
