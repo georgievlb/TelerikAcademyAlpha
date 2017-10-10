503c58d3db67da8d93267fdb18a09ec236e3877c
georgievlb <georgiev.lb@gmail.com>
2017-10-10 09:15:26 +0300

    Added minor changes

---
+++ Module 3/EntityFramework/CodeFirstExistingDatabase/CodeFirstExistingDatabase/PlutoContext.cs
@@ -0,0 +1,33 @@
+namespace CodeFirstExistingDatabase
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class PlutoContext : DbContext
+    {
+        public PlutoContext()
+            : base("name=PlutoContext")
+        {
+        }
+
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Courses> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)    //override conventions
+        {
+            modelBuilder.Entity<Author>()
+                .HasMany(e => e.Courses)
+                .WithOptional(e => e.Author)
+                .HasForeignKey(e => e.author_Id);
+
+            modelBuilder.Entity<Courses>()
+                .HasMany(e => e.Tags)
+                .WithMany(e => e.Courses)
+                .Map(m => m.ToTable("TagCourses").MapLeftKey("Course_Id"));
+        }
+    }
+}
