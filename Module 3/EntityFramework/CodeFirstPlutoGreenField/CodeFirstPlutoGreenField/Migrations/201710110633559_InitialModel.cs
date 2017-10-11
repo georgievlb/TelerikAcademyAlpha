257548f110276a599e8426fcb261c6698d4d825b
georgievlb <georgiev.lb@gmail.com>
2017-10-11 09:43:11 +0300

    Pluto green field code first initial commit

---
+++ Module 3/EntityFramework/CodeFirstPlutoGreenField/CodeFirstPlutoGreenField/Migrations/201710110633559_InitialModel.cs
@@ -0,0 +1,72 @@
+namespace CodeFirstPlutoGreenField.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(//EF uses convention over configuration so everything below is based on our model
+                "dbo.Authors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),//identiy column, primary key, id property
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        Level = c.Int(nullable: false),
+                        FullPrice = c.Single(nullable: false),
+                        Author_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Authors", t => t.Author_Id)
+                .Index(t => t.Author_Id);//so we can look up courses by author
+            
+            CreateTable(
+                "dbo.Tags",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.TagCourses",//intermediary table, used in many-to-many relationships
+                c => new         //because a course can have multiple tags, and a tag can
+                    {            //be applied to multiple courses
+                        Tag_Id = c.Int(nullable: false),
+                        Course_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Tag_Id, t.Course_Id })//an anonymous object with 2 properties - Tag_Id and Course_Id. So the primary key in this table is a composite key
+                .ForeignKey("dbo.Tags", t => t.Tag_Id, cascadeDelete: true)//cascadeDelete - if you delete sth from the tag table, it's also going to get deleted from the intermediary table
+                .ForeignKey("dbo.Courses", t => t.Course_Id, cascadeDelete: true)//same as above
+                .Index(t => t.Tag_Id)
+                .Index(t => t.Course_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.TagCourses", "Course_Id", "dbo.Courses");
+            DropForeignKey("dbo.TagCourses", "Tag_Id", "dbo.Tags");
+            DropForeignKey("dbo.Courses", "Author_Id", "dbo.Authors");
+            DropIndex("dbo.TagCourses", new[] { "Course_Id" });
+            DropIndex("dbo.TagCourses", new[] { "Tag_Id" });
+            DropIndex("dbo.Courses", new[] { "Author_Id" });
+            DropTable("dbo.TagCourses");
+            DropTable("dbo.Tags");
+            DropTable("dbo.Courses");
+            DropTable("dbo.Authors");
+        }
+    }
+}
