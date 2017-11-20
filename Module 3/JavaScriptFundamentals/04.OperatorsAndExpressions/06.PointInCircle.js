152d999d3e174e1ab0fabe75c0fa4d69ffca6c31
[georgievlb] <[georgiev.lb@gmail.com]>
2017-11-20 14:40:29 +0200

    Added trapezoid area problem + point in circle fix

--- Module 3/JavaScriptFundamentals/04.OperatorsAndExpressions/06.PointInCircle.js
+++ Module 3/JavaScriptFundamentals/04.OperatorsAndExpressions/06.PointInCircle.js
@@ -1,16 +1,16 @@
 function PointInCircle(array){
-    var xcoord = array[0];
-    var ycoord = array[1];
+    var xcoord = parseFloat(array[0]);
+    var ycoord = parseFloat(array[1]);
     var xcenter = 0;
     var ycenter = 0;
     var radius = 2;
-    var temp = (xcoord - xcenter) * (xcoord - xcenter) + 
-        (ycoord - ycenter) * (ycoord - ycenter);
-    var distance = Math.sqrt(temp);
+    var distance = Math.sqrt((xcoord - xcenter) * (xcoord - xcenter) + 
+        (ycoord - ycenter) * (ycoord - ycenter));
+    
     if(distance <= radius){
         return ("yes " + distance.toFixed(2));
     }
     else
-    return ("no" + distance.toFixed(2));
+    return ("no " + distance.toFixed(2));
 }
 
---
+++ Module 3/JavaScriptFundamentals/04.OperatorsAndExpressions/08.Trapezoids.js
@@ -0,0 +1,8 @@
+function TrapezoidArea(array){
+    var base1 = parseFloat(array[0]);
+    var base1 = parseFloat(array[1]);
+    var base1 = parseFloat(array[2]);
+    var area = (base1 + base2) / 2 * height;
+    
+    return area.toFixed(7);
+}
