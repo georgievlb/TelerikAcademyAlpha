3f41b978fd01e2a88fe08e720e3d64be895ed456
[georgievlb] <[georgiev.lb@gmail.com]>
2017-11-29 00:22:30 +0200

    Added prime numbers

--- Module 3/JavaScriptFundamentals/07.Arrays/10.PrimaryNumbers.js
+++ Module 3/JavaScriptFundamentals/07.Arrays/10.PrimaryNumbers.js
@@ -1,19 +1,30 @@
 function prime(number){
     var array = [];
-    for(var i = 0; i <= number; i++)
+    // for(var i = 0; i < number; i++)
+    // {
+    //     array.push(true);
+    // }
+    //Math.sqrt(number) is the maximum dividier of number
+    for(var i = 2; i <= Math.sqrt(number); i++)
     {
-        array[i] = true;
-    }
-
-    for(var i = 2; i < Math.sqrt(number); i++)
-    {
-        if(array[i] == true){
-            for(var j = i * i; j < number;){
-                array[j] = false;
+        if(array[i] == undefined){
+            for(var j = i * i; j <= number; j += i){
+                array[j] = true;
             }
         }
     }
 
+    var result = [];
+    for(var i = number; i >= 2; i--){
+        if(!array[i]){
+            return +i;//add a + so it's not treated as a string
+        }
+    }
+    // for(var i = 2; i < number; i++){
+    //     if(array[i] == undefined){ //== true
+    //     result.push(i);
+    //     }
+    // }
 
-    return array;
+    //return result.pop();
 }
